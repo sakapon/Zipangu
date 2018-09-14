686ba4fb1c96ed6748ee57ecfb5fd567d626cf29
Keiho Sakapon <git@saka-pon.net>
2018-09-14 15:43:36 +0900

    Add test

--- Zipangu/NetUnitTest/KanaConversionTest.cs
+++ Zipangu/NetUnitTest/KanaConversionTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualBasic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Zipangu;
@@ -15,6 +16,25 @@ namespace NetUnitTest
 
             Test("");
             Test(KanaConversion.Asciis);
+        }
+
+        [TestMethod]
+        public void ToHiragana_Null()
+        {
+            Assert.IsNull(default(string).ToHiragana());
+        }
+
+        [TestMethod]
+        public void ToHiragana_Original()
+        {
+            var changed = Enumerable.Range(0, char.MaxValue + 1)
+                .Select(i => new { i, before = (char)i, after = ((char)i).ToString().ToHiragana_VB().Single() })
+                .Where(_ => _.before != _.after)
+                .Where(_ => _.after != '？' || _.before == '?')
+                .ToArray();
+
+            foreach (var _ in changed)
+                Console.WriteLine($"{_.i:D5}: {_.before} > {_.after}");
         }
     }
 
