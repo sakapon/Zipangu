e30f28cb47608ef18bbdd9a794e5edc6aa6e6cfc
Keiho Sakapon <git@saka-pon.net>
2018-09-16 19:32:28 +0900

    Add test

--- Zipangu/NetUnitTest/AsciiConversionTest.cs
+++ Zipangu/NetUnitTest/AsciiConversionTest.cs
@@ -13,7 +13,7 @@ namespace NetUnitTest
         {
             var Test = TestHelper.CreateAssertion<string, string>(AsciiConversion.ToWideAscii);
 
-            Assert.IsNull(default(string).ToWideAscii());
+            Test(null, null);
             Test("", "");
             Test(AsciiConversion.Asciis, AsciiConversion.WideAsciis);
 
@@ -32,8 +32,38 @@ namespace NetUnitTest
             Test("");
             Test(AsciiConversion.Asciis.Replace(@"\", ""));
 
+            Test_VB(null, "");
             // "\" は変換されません。
             Test_VB(@"\", @"\");
+        }
+
+        [TestMethod]
+        public void ToNarrowAscii()
+        {
+            var Test = TestHelper.CreateAssertion<string, string>(AsciiConversion.ToNarrowAscii);
+
+            Test(null, null);
+            Test("", "");
+            Test(AsciiConversion.WideAsciis, AsciiConversion.Asciis);
+
+            var chars = EnumerableHelper.RangeChars(char.MinValue, char.MaxValue)
+                .Where(c => !AsciiConversion.WideAsciis.Contains(c));
+            foreach (var c in chars)
+                Test(c.ToString(), c.ToString());
+        }
+
+        [TestMethod]
+        public void ToNarrowAscii_VB()
+        {
+            void Test(string value) => Assert.AreEqual(value.ToNarrow_VB(), value.ToNarrowAscii());
+            var Test_VB = TestHelper.CreateAssertion<string, string>(VBStringsHelper.ToNarrow_VB);
+
+            Test("");
+            Test(AsciiConversion.WideAsciis.Replace("＼", ""));
+
+            Test_VB(null, "");
+            // "＼" は変換されません。
+            Test_VB("＼", "＼");
         }
     }
 }
