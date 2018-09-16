66e5fe2d8b31ce0c1a6c03c08148feb1bf2a2a1c
Keiho Sakapon <git@saka-pon.net>
2018-09-16 19:10:32 +0900

    Add test

--- Zipangu/NetUnitTest/AsciiConversionTest.cs
+++ Zipangu/NetUnitTest/AsciiConversionTest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zipangu;
 
 namespace NetUnitTest
 {
@@ -7,8 +9,31 @@ namespace NetUnitTest
     public class AsciiConversionTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ToWideAscii()
         {
+            var Test = TestHelper.CreateAssertion<string, string>(AsciiConversion.ToWideAscii);
+
+            Assert.IsNull(default(string).ToWideAscii());
+            Test("", "");
+            Test(AsciiConversion.Asciis, AsciiConversion.WideAsciis);
+
+            var chars = EnumerableHelper.RangeChars(char.MinValue, char.MaxValue)
+                .Replace(AsciiConversion.Asciis, "");
+            foreach (var c in chars)
+                Test(c.ToString(), c.ToString());
+        }
+
+        [TestMethod]
+        public void ToWideAscii_VB()
+        {
+            void Test(string value) => Assert.AreEqual(value.ToWide_VB(), value.ToWideAscii());
+            var Test_VB = TestHelper.CreateAssertion<string, string>(VBStringsHelper.ToWide_VB);
+
+            Test("");
+            Test(AsciiConversion.Asciis.Replace(@"\", ""));
+
+            // "\" は変換されません。
+            Test_VB(@"\", @"\");
         }
     }
 }
