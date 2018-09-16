439766706dbf07190f68985f3e3540598657f9c2
Keiho Sakapon <git@saka-pon.net>
2018-09-16 18:09:32 +0900

    Implement methods

--- Zipangu/Zipangu/AsciiConversion.cs
+++ Zipangu/Zipangu/AsciiConversion.cs
@@ -17,7 +17,21 @@ namespace Zipangu
         static readonly IDictionary<char, char> WideMap = Asciis.ZipToDictionary(WideAsciis);
         static readonly IDictionary<char, char> NarrowMap = WideAsciis.ZipToDictionary(Asciis);
 
-        internal static char ToWide(char c) => WideMap.ContainsKey(c) ? WideMap[c] : c;
-        internal static char ToNarrow(char c) => NarrowMap.ContainsKey(c) ? NarrowMap[c] : c;
+        internal static char ToWideAscii(char c) => WideMap.ContainsKey(c) ? WideMap[c] : c;
+        internal static char ToNarrowAscii(char c) => NarrowMap.ContainsKey(c) ? NarrowMap[c] : c;
+
+        /// <summary>
+        /// 半角英数字記号 (ASCII 文字) を全角に変換します。
+        /// </summary>
+        /// <param name="value">変換対象の文字列。</param>
+        /// <returns>変換後の文字列。</returns>
+        public static string ToWideAscii(this string value) => value == null ? null : string.Concat(value.Select(ToWideAscii));
+
+        /// <summary>
+        /// 全角英数字記号を半角 (ASCII 文字) に変換します。
+        /// </summary>
+        /// <param name="value">変換対象の文字列。</param>
+        /// <returns>変換後の文字列。</returns>
+        public static string ToNarrowAscii(this string value) => value == null ? null : string.Concat(value.Select(ToNarrowAscii));
     }
 }
