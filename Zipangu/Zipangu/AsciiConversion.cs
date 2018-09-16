252b0f4d9848a5d019e3187d48e8a34e3564040e
Keiho Sakapon <git@saka-pon.net>
2018-09-16 17:02:15 +0900

    Add classes

---
+++ Zipangu/Zipangu/AsciiConversion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Zipangu
+{
+    /// <summary>
+    /// ASCII 文字を変換するためのメソッドを提供します。
+    /// </summary>
+    public static class AsciiConversion
+    {
+        /// <summary>半角英数字記号 (ASCII 文字)。</summary>
+        internal static readonly string Asciis = EnumerableHelper.RangeChars(' ', '~');
+        /// <summary>全角英数字記号。</summary>
+        internal static readonly string WideAsciis = "　" + EnumerableHelper.RangeChars('！', '～');
+
+        static readonly IDictionary<char, char> WideMap = Asciis.ZipToDictionary(WideAsciis);
+        static readonly IDictionary<char, char> NarrowMap = WideAsciis.ZipToDictionary(Asciis);
+
+        internal static char ToWide(char c) => WideMap.ContainsKey(c) ? WideMap[c] : c;
+        internal static char ToNarrow(char c) => NarrowMap.ContainsKey(c) ? NarrowMap[c] : c;
+    }
+}
---
+++ Zipangu/Zipangu/EnumerableHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Zipangu
+{
+    static class EnumerableHelper
+    {
+        public static Dictionary<TKey, TValue> ZipToDictionary<TKey, TValue>(this IEnumerable<TKey> keys, IEnumerable<TValue> values) =>
+            keys.Zip(values, (x, y) => new { x, y }).ToDictionary(_ => _.x, _ => _.y);
+
+        public static string RangeChars(char start, char end) =>
+            string.Concat(Enumerable.Range(start, end - start + 1).Select(i => (char)i));
+    }
+}
