85211fc670a5fe41bf107ced106532011b0b550b
Keiho Sakapon <git@saka-pon.net>
2018-09-17 18:12:14 +0900

    Add test

--- Zipangu/NetUnitTest/KanaConversionTest.cs
+++ Zipangu/NetUnitTest/KanaConversionTest.cs
@@ -42,6 +42,37 @@ namespace NetUnitTest
         }
 
         [TestMethod]
+        public void HalfKatakanaToKatakana()
+        {
+            var Test = TestHelper.CreateAssertion<string, string>(KanaConversion.HalfKatakanaToKatakana);
+
+            Test(null, null);
+            Test("", "");
+            Test(KanaConversion.HalfKatakanas, KanaConversion.KatakanasByHalfKatakana);
+            Test(string.Concat(KanaConversion.VoiceableHalfKatakanas.Select(c => $"{c}ﾞ")), KanaConversion.VoicedKatakanas);
+            Test(string.Concat(KanaConversion.SemiVoiceableHalfKatakanas.Select(c => $"{c}ﾟ")), KanaConversion.SemiVoicedKatakanas);
+
+            var chars = EnumerableHelper.RangeChars(char.MinValue, char.MaxValue)
+                .Replace(KanaConversion.HalfKatakanas, "");
+            foreach (var c in chars)
+                Test(c.ToString(), c.ToString());
+        }
+
+        [TestMethod]
+        public void HalfKatakanaToKatakana_VB()
+        {
+            void Test(string value) => Assert.AreEqual(value.ToWideKatakana_VB(), value.HalfKatakanaToKatakana());
+            var Test_VB = TestHelper.CreateAssertion<string, string>(VBStringsHelper.ToWideKatakana_VB);
+
+            Test("");
+            Test(KanaConversion.HalfKatakanas);
+            Test(string.Concat(KanaConversion.VoiceableHalfKatakanas.Select(c => $"{c}ﾞ")));
+            Test(string.Concat(KanaConversion.SemiVoiceableHalfKatakanas.Select(c => $"{c}ﾟ")));
+
+            Test_VB(null, "");
+        }
+
+        [TestMethod]
         public void ToHiragana_Original()
         {
             var changed = Enumerable.Range(0, char.MaxValue + 1)
