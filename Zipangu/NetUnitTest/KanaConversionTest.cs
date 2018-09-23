59561b72a016eadda5cecaf1c1a23208f4d5be77
Keiho Sakapon <git@saka-pon.net>
2018-09-23 11:57:57 +0900

    Refactor

--- Zipangu/NetUnitTest/KanaConversionTest.cs
+++ Zipangu/NetUnitTest/KanaConversionTest.cs
@@ -75,18 +75,5 @@ namespace NetUnitTest
 
             Test_VB(null, "");
         }
-
-        [TestMethod]
-        public void ToWideHiragana_Original()
-        {
-            var changed = Enumerable.Range(0, char.MaxValue + 1)
-                .Select(i => new { i, before = (char)i, after = ((char)i).ToString().ToWideHiragana_VB().Single() })
-                .Where(_ => _.before != _.after)
-                .Where(_ => _.after != '？' || _.before == '?')
-                .ToArray();
-
-            foreach (var _ in changed)
-                Console.WriteLine($"{_.i:D5}-{_.i:X4}: {_.before} > {_.after}");
-        }
     }
 }
---
+++ Zipangu/NetUnitTest/VBStringsTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NetUnitTest
+{
+    [TestClass]
+    public class VBStringsTest
+    {
+        [TestMethod]
+        public void ToWideHiragana_Original()
+        {
+            var changed = Enumerable.Range(0, char.MaxValue + 1)
+                .Select(i => new { i, before = (char)i, after = ((char)i).ToString().ToWideHiragana_VB().Single() })
+                .Where(_ => _.before != _.after)
+                .Where(_ => _.after != '？' || _.before == '?')
+                .ToArray();
+
+            foreach (var _ in changed)
+                Console.WriteLine($"{_.i:D5}-{_.i:X4}: {_.before} > {_.after}");
+        }
+    }
+}
