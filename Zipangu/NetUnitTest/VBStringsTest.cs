0e640b85b126676ab79fefdd20076445e548cab8
Keiho Sakapon <git@saka-pon.net>
2018-09-25 00:17:58 +0900

    Add test

--- Zipangu/NetUnitTest/VBStringsTest.cs
+++ Zipangu/NetUnitTest/VBStringsTest.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
+using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zipangu;
 
 namespace NetUnitTest
 {
     [TestClass]
     public class VBStringsTest
     {
+        static string ToMessage1(char c) => $"({(int)c:D5}-{(int)c:X4}) {c}";
+        static string ToMessage2(char c) => $"{c} ({(int)c:D5}-{(int)c:X4})";
+
+        [TestMethod]
+        public void Katakana()
+        {
+            var changed = EnumerableHelper.RangeChars(char.MinValue, char.MaxValue)
+                .Select(c => new { before = c, after = c.ToString().ToKatakana_VB().Single() })
+                .Where(_ => _.before != _.after)
+                .Where(_ => _.after != '?')
+                .Select(_ => $"{ToMessage1(_.before)} > {ToMessage2(_.after)}")
+                .ToArray();
+
+            File.WriteAllLines("VBStrings-Katakana.txt", changed, Encoding.UTF8);
+        }
+
         [TestMethod]
         public void ToWideHiragana_Original()
         {
