32efeec823a1938c41804953d455b4c759568db0
Keiho Sakapon <git@saka-pon.net>
2018-09-26 16:17:03 +0900

    Refactor

--- Zipangu/NetUnitTest/VBStringsTest.cs
+++ Zipangu/NetUnitTest/VBStringsTest.cs
@@ -14,19 +14,6 @@ namespace NetUnitTest
         static string ToMessage1(char c) => $"({(int)c:D5}-{(int)c:X4}) {c}";
         static string ToMessage2(char c) => $"{c} ({(int)c:D5}-{(int)c:X4})";
 
-        static void WriteChanged(VbStrConv mode)
-        {
-            // 他言語の文字は "?" に変換されます。
-            var changed = EnumerableHelper.RangeChars(char.MinValue, char.MaxValue)
-                .Select(c => new { before = c, after = Strings.StrConv(c.ToString(), mode).Single() })
-                .Where(_ => _.before != _.after)
-                .Where(_ => _.after != '?')
-                .Select(_ => $"{ToMessage1(_.before)} > {ToMessage2(_.after)}")
-                .ToArray();
-
-            File.WriteAllLines($"VBStrings-{mode.ToString().Replace(", ", "")}.txt", changed, Encoding.UTF8);
-        }
-
         static void WriteChanged_Wide(VbStrConv mode)
         {
             // 他言語の文字は "？" に変換されます。
@@ -60,10 +47,10 @@ namespace NetUnitTest
         public void Wide() => WriteChanged_Wide(VbStrConv.Wide);
 
         [TestMethod]
-        public void Katakana() => WriteChanged(VbStrConv.Katakana);
+        public void Katakana() => WriteChanged_Narrow(VbStrConv.Katakana);
 
         [TestMethod]
-        public void Hiragana() => WriteChanged(VbStrConv.Hiragana);
+        public void Hiragana() => WriteChanged_Narrow(VbStrConv.Hiragana);
 
         [TestMethod]
         public void NarrowKatakana() => WriteChanged_Narrow(VbStrConv.Narrow | VbStrConv.Katakana);
