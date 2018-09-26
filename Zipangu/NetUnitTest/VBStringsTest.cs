918dec7a8aefbafa356aaac7110c162df436a9ac
Keiho Sakapon <git@saka-pon.net>
2018-09-26 13:38:40 +0900

    Add test

--- Zipangu/NetUnitTest/VBStringsTest.cs
+++ Zipangu/NetUnitTest/VBStringsTest.cs
@@ -27,6 +27,22 @@ namespace NetUnitTest
             File.WriteAllLines($"VBStrings-{mode}.txt", changed, Encoding.UTF8);
         }
 
+        static void WriteChanged_Wide(VbStrConv mode)
+        {
+            // 他言語の文字は "？" に変換されます。
+            var changed = EnumerableHelper.RangeChars(char.MinValue, char.MaxValue)
+                .Select(c => new { before = c, after = Strings.StrConv(c.ToString(), mode).Single() })
+                .Where(_ => _.before != _.after)
+                .Where(_ => _.after != '？' || _.before == '?')
+                .Select(_ => $"{ToMessage1(_.before)} > {ToMessage2(_.after)}")
+                .ToArray();
+
+            File.WriteAllLines($"VBStrings-{mode}.txt", changed, Encoding.UTF8);
+        }
+
+        [TestMethod]
+        public void Wide() => WriteChanged_Wide(VbStrConv.Wide);
+
         [TestMethod]
         public void Katakana() => WriteChanged(VbStrConv.Katakana);
 
