72032897de62caffbafd99d85e80edbee1a443da
Keiho Sakapon <git@saka-pon.net>
2018-09-26 12:22:10 +0900

    Refactor

--- Zipangu/NetUnitTest/VBStringsTest.cs
+++ Zipangu/NetUnitTest/VBStringsTest.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Linq;
 using System.Text;
+using Microsoft.VisualBasic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Zipangu;
 
@@ -13,18 +14,24 @@ namespace NetUnitTest
         static string ToMessage1(char c) => $"({(int)c:D5}-{(int)c:X4}) {c}";
         static string ToMessage2(char c) => $"{c} ({(int)c:D5}-{(int)c:X4})";
 
-        [TestMethod]
-        public void Katakana()
+        static void WriteChanged(VbStrConv mode)
         {
+            // 他言語の文字は "?" に変換されます。
             var changed = EnumerableHelper.RangeChars(char.MinValue, char.MaxValue)
-                .Select(c => new { before = c, after = c.ToString().ToKatakana_VB().Single() })
+                .Select(c => new { before = c, after = Strings.StrConv(c.ToString(), mode).Single() })
                 .Where(_ => _.before != _.after)
                 .Where(_ => _.after != '?')
                 .Select(_ => $"{ToMessage1(_.before)} > {ToMessage2(_.after)}")
                 .ToArray();
 
-            File.WriteAllLines("VBStrings-Katakana.txt", changed, Encoding.UTF8);
+            File.WriteAllLines($"VBStrings-{mode}.txt", changed, Encoding.UTF8);
         }
+
+        [TestMethod]
+        public void Katakana() => WriteChanged(VbStrConv.Katakana);
+
+        [TestMethod]
+        public void Hiragana() => WriteChanged(VbStrConv.Hiragana);
 
         [TestMethod]
         public void ToWideHiragana_Original()
