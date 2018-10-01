f17ce1f50f1fcd0c366bc758786a5900a7cd29c9
Keiho Sakapon <git@saka-pon.net>
2018-10-01 15:36:12 +0900

    Add class for encoding

---
+++ Zipangu/Zipangu/EncodingHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+namespace Zipangu
+{
+    public static class EncodingHelper
+    {
+        public static Encoding ShiftJIS { get; }
+
+        static EncodingHelper()
+        {
+#if NETSTANDARD
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+#endif
+            ShiftJIS = Encoding.GetEncoding("shift_jis");
+        }
+    }
+}
