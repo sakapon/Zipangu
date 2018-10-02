4d2972f9bd6a438cbf02d3a6658380eb5ab376f8
Keiho Sakapon <git@saka-pon.net>
2018-10-02 15:42:47 +0900

    Add test

--- Zipangu/UnitTest/EncodingHelperTest.cs
+++ Zipangu/UnitTest/EncodingHelperTest.cs
@@ -40,5 +40,26 @@ namespace UnitTest
             Assert.AreEqual(text, EncodingHelper.ShiftJIS.GetString(bytes));
             CollectionAssert.AreEqual(bytes, EncodingHelper.ShiftJIS.GetBytes(text));
         }
+
+        [TestMethod]
+        public void ISO2022JP_GetString()
+        {
+            // 半角カタカナは存在しません。
+            var text = "シャ乱Q";
+            var bytes = new byte[] { 27, 36, 66, 37, 55, 37, 99, 77, 112, 27, 40, 66, 81 };
+
+            Assert.AreEqual(text, EncodingHelper.ISO2022JP.GetString(bytes));
+            CollectionAssert.AreEqual(bytes, EncodingHelper.ISO2022JP.GetBytes(text));
+        }
+
+        [TestMethod]
+        public void EUCJP_GetString()
+        {
+            var text = "ｼｬ乱Q";
+            var bytes = new byte[] { 142, 188, 142, 172, 205, 240, 81 };
+
+            Assert.AreEqual(text, EncodingHelper.EUCJP.GetString(bytes));
+            CollectionAssert.AreEqual(bytes, EncodingHelper.EUCJP.GetBytes(text));
+        }
     }
 }
